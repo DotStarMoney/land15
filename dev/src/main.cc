43426551164231bad854f6bd15eaaa40a3fcffe3
DotStarMoney <Batterybat@gmail.com>
2023-09-16 18:57:21 -0400

    Graphics library ready!

--- dev/src/main.cc
+++ dev/src/main.cc
@@ -1,7 +1,18 @@
 #include "glog/logging.h"
+#include "gfx/gfx.h"
+
 
 int main(int argc, char* argv[]) {
   google::InitGoogleLogging(argv[0]);
+
+  land15::gfx::Gfx::Screen({640, 480});
+
+  while (!land15::gfx::Gfx::Close()) {
+    land15::gfx::Gfx::SyncInputs();
+
+    land15::gfx::Gfx::TextLine("Old Country Buffet", {0, 0});
+    land15::gfx::Gfx::Flip();
+  }
 
   return 0;
 }
